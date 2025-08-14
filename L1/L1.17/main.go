b8232f2ddd3b59149ea8cf7b0bbcb03bfbcb4694
golovanevvs <lony@inbox.ru>
2025-08-14 22:06:16 +0300

    L1: L1.17: done

---
+++ L1/L1.17/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+)
+
+func main() {
+	n := 20
+	m := 100
+	data := make([]int, 0, n)
+	seen := make(map[int]struct{})
+	for len(data) < n {
+		v := rand.Intn(m)
+		if _, exists := seen[v]; !exists {
+			seen[v] = struct{}{}
+			data = append(data, v)
+		}
+	}
+	sort.Ints(data)
+	fmt.Printf("data: %v\n", data)
+
+	var targetValue int
+	fmt.Println("Enter the target value:")
+	fmt.Scan(&targetValue)
+
+	index := search(data, targetValue)
+	if index == -1 {
+		fmt.Printf("Target value %d not found\n", targetValue)
+		return
+	}
+
+	fmt.Printf("The index of the taget value %d is %d\n", targetValue, index)
+}
+
+func search(data []int, targetValue int) int {
+	left, right := 0, len(data)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		switch {
+		case data[mid] == targetValue:
+			return mid
+		case data[mid] > targetValue:
+			right = mid - 1
+		default:
+			left = mid + 1
+		}
+	}
+
+	return -1
+}
