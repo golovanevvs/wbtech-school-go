d677f2aba416031fdeba287f33774db6b9a23fc6
golovanevvs <lony@inbox.ru>
2025-09-09 23:59:53 +0300

    L2: L2.12: dev: sync

---
+++ L2/L2.12/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+type options struct {
+	afterContextCount  int  // -A N
+	beforeContextCount int  // -B N
+	aroundContextCount int  // -C N
+	onlyCount          bool // -c
+	ignoreCase         bool // -i
+	invertMatch        bool // -v
+	fixedString        bool // -F
+	lineNumber         bool // -n
+}
+
+func main() {
+	opts := newOptions()
+
+	rootCmd := &cobra.Command{
+		Use:   "vgrep",
+		Short: "vgrep - v grep",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return run(args, opts)
+		},
+	}
+
+	rootCmd.Flags().IntVar(&opts.afterContextCount, "A", 0, "Print N lines of context after each matching line")
+	rootCmd.Flags().IntVar(&opts.beforeContextCount, "B", 0, "Print N lines of context before each matching line")
+	rootCmd.Flags().IntVar(&opts.aroundContextCount, "C", 0, "Print N lines of context around each matching line")
+	rootCmd.Flags().BoolVar(&opts.onlyCount, "c", false, "Print only the count of matching lines")
+	rootCmd.Flags().BoolVar(&opts.ignoreCase, "i", false, "Ignore case")
+	rootCmd.Flags().BoolVar(&opts.invertMatch, "v", false, "Invert the match")
+	rootCmd.Flags().BoolVar(&opts.fixedString, "F", false, "Treat the pattern as a fixed string")
+	rootCmd.Flags().BoolVar(&opts.lineNumber, "n", false, "Print line numbers")
+
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+
+}
+
+func newOptions() *options {
+	return &options{}
+}
+
+func run(args []string, opts *options) error {
+	return nil
+}
