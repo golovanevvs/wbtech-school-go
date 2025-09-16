702fadfb5494ea8e25c6e6c19c6b9e40d51fd859
golovanevvs <lony@inbox.ru>
2025-09-16 17:12:37 +0300

    L2: L2.18: dev: sync

--- L2/L2.18/cal/internal/model/model.go
+++ L2/L2.18/cal/internal/model/model.go
@@ -1,6 +1,8 @@
 package model
 
 type Event struct {
-	id   int
-	name string
+	Id      int    `json:"id"`
+	Title   string `json:"title"`
+	Comment string `json:"comment"`
+	Date    string `json:"date"`
 }
