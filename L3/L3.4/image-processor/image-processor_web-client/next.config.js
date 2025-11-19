0b0bb4e82e4a24b321b816498c155b8a18dc46bb
golovanevvs <lony@inbox.ru>
2025-11-19 15:12:28 +0300

    L3: L3.4: dev: sync

--- L3/L3.4/image-processor/image-processor_web-client/next.config.js
+++ L3/L3.4/image-processor/image-processor_web-client/next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  allowedDevOrigins: [
-    "reflectively-credited-lorikeet.cloudpub.ru",
-    "*.cloudpub.ru",
-  ],
   reactCompiler: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
-  reactStrictMode: false,
 }
 
 module.exports = nextConfig
