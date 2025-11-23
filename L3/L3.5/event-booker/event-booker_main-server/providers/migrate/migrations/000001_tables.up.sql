a85cea1cd97f0c7eabc1a45840d1d44a6d8f40a2
golovanevvs <lony@inbox.ru>
2025-11-23 17:09:21 +0300

    L3: L3.5: dev: sync

--- L3/L3.5/event-booker/event-booker_main-server/providers/migrate/migrations/000001_tables.up.sql
+++ L3/L3.5/event-booker/event-booker_main-server/providers/migrate/migrations/000001_tables.up.sql
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS events (
     total_places INTEGER NOT NULL,
     available_places INTEGER NOT NULL,
     booking_deadline INTEGER NOT NULL,
-    owner_id INTEGER REFERENCES users(id),
+    owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
